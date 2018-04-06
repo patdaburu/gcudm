15c607a7b35cc842d19dcf8d564bc6f2659858ee
patdaburu <pat@daburu.net>
2018-04-06 05:35:19 -0500

    Successful run with usage table.

--- gcudm/docstrings.py
+++ gcudm/docstrings.py
@@ -9,28 +9,33 @@
 This module contains docstring generators.
 """
 
-from .meta import ColumnMeta, COLUMN_META_ATTR
+from .meta import ColumnMeta, COLUMN_META_ATTR, Usage, Requirement
 import inspect
 from functools import wraps
 from sqlalchemy import Column
-from typing import Set, Tuple
+from typing import List, Tuple
 
 
 def model(cls):
-    # We're going to go find all the members within the class hierarcy that
+    # We're going to go find all the members within the class hierarchy that
     # seem to be columns with metadata.
-    column_members: Set[Tuple[str, Column]] = set()
+    column_members: List[Tuple[str, Column]] = []
     # Let's go through every class in the hierarchy...
     for mro in inspect.getmro(cls):
         # ...updating our list with information about all the members.
-        column_members.update(
+        column_members.extend(
             [
                 member for member in inspect.getmembers(mro)
                 if hasattr(member[1], COLUMN_META_ATTR)
             ]
         )
+    # Eliminate duplicates.
+    column_members = list(set(column_members))
+    column_members.sort(key=lambda i: i[0])
 
-    nx = [x[0] for x in column_members]
+
+
+    #nx = [x[0] for x in column_members]
     # for nnn in nx:
     #     print(nnn)
 
@@ -43,7 +48,35 @@ def model(cls):
     return cls
 
 
+def fmt_rst(s: str, indent: int=1, wrap: bool=True):
+    return '{}{}{}'.format('\t'*indent, s, '\n' if wrap else '')
+
+
+
 def to_rst(member_name: str, meta: ColumnMeta):
-    lines = [f'*{member_name}*']
-    lines.append('\tMore info...\n')
-    return '\n'.join(lines)
+    lines = [f'**{member_name}** - *{meta.label}*']
+    if meta.nena is not None:
+        lines.append(fmt_rst(f'NENA: *{meta.nena}*'))
+
+    lines.append(fmt_rst('Usage'))
+    excluded = {Usage.NONE}
+    vals = [v for v in Usage if v not in excluded]
+    tbl_borders = [None] * len(vals)
+    tbl_headers = [None] * len(vals)
+    tbl_values  = [None] * len(vals)
+    for i in range(0, len(vals)):
+        enum_name = vals[i].name
+        tbl_borders[i] = '=' * len(enum_name)
+        tbl_headers[i] = enum_name
+        tbl_values[i] = f'✔{" " * len(enum_name)}'
+    lines.append(' '.join(tbl_borders))
+    lines.append(' '.join(tbl_headers))
+    lines.append(' '.join(tbl_borders))
+    lines.append(' '.join(tbl_values))
+    lines.append(' '.join(tbl_borders))
+
+
+    # Append a blank line to separate this section from the next one.
+    lines.append('')
+    rst = '\n'.join(lines)
+    return rst
